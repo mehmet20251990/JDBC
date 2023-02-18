6b1768e7680e71292b5ffc28af8d7e3034b77444
mehmet20251990 <mehmet20251990@gmail.com>
2023-02-18 21:39:41 +0300

    update

---
+++ src/CreateCallableStatement14.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+public class CreateCallableStatement14 {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        try{
+            // db parameters
+            String url       = "jdbc:mysql://localhost:3306/fsae01";
+            String user      = "root";
+            String password  = "Sims;1905";
+
+            // Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // create a connection to the database
+            Connection con = DriverManager.getConnection(url, user, password);
+            Statement stmt= con.createStatement();
+
+
+
+            con.close();
+        }catch(SQLException e){ System.out.println(e);}
+    }
+}
