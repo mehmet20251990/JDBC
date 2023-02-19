7fef6c1e468b5be1f4911fc6622231243841f848
mehmet20251990 <mehmet20251990@gmail.com>
2023-02-19 23:52:03 +0300

    update

--- src/CreateStatement5.java
+++ src/CreateStatement5.java
@@ -3,26 +3,31 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 public class CreateStatement5 {
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        try{
-            // db parameters
-            String url       = "jdbc:mysql://localhost:3306/fsae01";
-            String user      = "root";
-            String password  = "Sims;1905";
+	public CreateStatement5() {
+		// TODO Auto-generated constructor stub
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try{
+			// db parameters
+			String url       = "jdbc:mysql://localhost:3306/fsae01";
+			String user      = "root";
+			String password  = "Aa123456";			
 
-            // Class.forName("com.mysql.cj.jdbc.Driver");
+			// Class.forName("com.mysql.cj.jdbc.Driver");
 
-            // create a connection to the database
-            Connection con = DriverManager.getConnection(url, user, password);
-            Statement stmt= con.createStatement();
+			// create a connection to the database
+			Connection con = DriverManager.getConnection(url, user, password);
+			Statement stmt = con.createStatement();
+			int result=stmt.executeUpdate("create table personel(id char(4), isim varchar(40), maas int)");
 
-            String str = "create table ornekTable (id int , isim varchar(50));";
-            int create = stmt.executeUpdate(str);
-            System.out.println(create);
-
-            con.close();
-        }catch(SQLException e){ System.out.println(e);}
-    }
+			System.out.println(result + " records affected");
+			/*
+			String str = "create table ornekTable (id int , isim varchar(50));";
+			int create = stmt.executeUpdate(str);
+			System.out.println(create);
+			*/
+			con.close();
+		}catch(SQLException e){ System.out.println(e);}
+	}
 }
-
--- src/DeleteStatement4.java
+++ src/DeleteStatement4.java
@@ -12,19 +12,25 @@ public class DeleteStatement4 {
 			// db parameters
 			String url       = "jdbc:mysql://localhost:3306/fsae01";
 			String user      = "root";
-			String password  = "Sims;1905";
+			String password  = "Aa123456";			
 
 			// Class.forName("com.mysql.cj.jdbc.Driver");
 
 			// create a connection to the database
 			Connection con = DriverManager.getConnection(url, user, password);
-			Statement stmt= con.createStatement();
-			int result=stmt.executeUpdate("delete from ogrenciler where id = 120");
+			Statement stmt = con.createStatement();
+			/*
+			String isim = "'Yavuz Bal'";
+			String sqlStatement = "delete from ogrenciler where isim =" + isim;
+			int result = stmt.executeUpdate(sqlStatement);
+			*/
+			/*
+			int result = stmt.executeUpdate("update ogrenciler set isim = 'Martin Parr2' where id = 121");
+			*/
+			int result = stmt.executeUpdate("insert into ogrenciler values(221, 'Ali Veli2', 'Ankara', 95)");
+			System.out.println(result + " records affected");			
 
-			// int result=stmt.executeUpdate("update ogrenciler set isim = 'Martin Parr' where id = 120");
-			System.out.println(result+" records affected");			
-
-			con.close();  
+			con.close();
 		}catch(SQLException e){ System.out.println(e);}
 	}
 }
